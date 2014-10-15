4a8c086a75ad7e5214f0ee04c9b03b6d1384c15e
mer4 <mer4@njit.edu>
2014-10-15 19:03:20 -0400

    Create chal2_tableCreator.php

---
+++ chal2_tableCreator.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace progchal2;
+	
+class tableCreator{
+		
+  public static function showTable($records, $names){
+		if(isset($_GET['record'])){
+			echo '<table border="2">';
+			$i = 0;
+			foreach($records[$_GET['record']] as $key => $value){
+					echo'<tr><th>' . $names[$i]['varTitle'] . '</th>';
+					echo '<td>' . $value . '</td></tr>';
+					$i++;
+			}
+			echo'</table>';
+		}
+	}
+		
+	public static function links($prim, $sec, $thir){
+    echo '<a href="?' . $prim . '=' . $sec . '">' . $thir . '</a>';
+		echo'<br>';
+	}
+	
+	public static function showTitle($uni){
+		if(empty($_GET)){
+				echo '<h2 id="uni_name">Record</h1>';
+		} else{
+				echo '<h2 id="uni_name">' . $uni . '</h1>';
+		}
+	}
+}
+
+?>
