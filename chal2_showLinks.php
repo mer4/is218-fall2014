084dfce4cd5a95642b8d04dd5bf18d04b822a075
mer4 <mer4@njit.edu>
2014-10-15 19:09:45 -0400

    Update chal2_showLinks.php

--- chal2_showLinks.php
+++ chal2_showLinks.php
@@ -4,12 +4,12 @@ namespace progchal2;
 
 class showLinks{
 		
-		public function __construct($records){
+		public function __construct($records, $names){
 			$i = -1;
 			if(empty($_GET)){
 				foreach($records as $record){
 					$i++;
-					\progchal2::links('record',$i, $record['INSTNM']);
+					\progchal2::links('record', $i, $record['INSTNM']);
 				}
 			}
 		}
