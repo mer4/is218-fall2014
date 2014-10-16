4197d32ebc815f10347528adb893623217495a07
mer4 <mer4@njit.edu>
2014-10-15 20:49:33 -0400

    Update chal2_autoloading.php

--- chal2_autoloading.php
+++ chal2_autoloading.php
@@ -11,7 +11,8 @@ class chal2_autoloading{
 			$className = substr($className, $lastNsPos + 1);
 			$fileName  = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
 		}
-		$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+		//$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+		$fileName .= $className . '.php';
 		require $fileName;
 	}
 }
